cb93e0c1e76776b445319eb8012fdf6927c9d70b
LarrySimiyu <larry.simiyu@gmail.com>
2019-07-17 22:05:36 -0700

    hit list get router

--- api/routes/hitList-router.js
+++ api/routes/hitList-router.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models/hitList-model");
+router.use(express.json());
+
+router.get("/", async (req, res) => {
+  try {
+    const title = await db.find();
+    res.status(200).json(title);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
 
 module.exports = router;
