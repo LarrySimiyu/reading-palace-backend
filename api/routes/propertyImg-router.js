ed6e56ced8a0caf910056c87714fd164b383c7cb
LarrySimiyu <larry.simiyu@gmail.com>
2019-07-29 11:38:00 -0700

    put endpoint added

--- api/routes/propertyImg-router.js
+++ api/routes/propertyImg-router.js
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const image = await db.findById();
+    const image = await db.findById(req.params.id);
     if (image) {
       res.status(200).json(image);
     } else {
@@ -26,10 +26,27 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const image = await db.add();
+    const image = await db.add(req.body);
     res.status(200).json(image);
   } catch (error) {
     res.status(500).json(error.message);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const image = await db.update(req.params.id, req.body);
+    if (updated) {
+      res.status(200).json(updated);
+    } else {
+      res.status(500).json({
+        message: "id not found"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating"
+    });
   }
 });
 
