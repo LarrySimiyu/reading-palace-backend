9235f7ba8175880a86cecbff206537e8832cf9ec
LarrySimiyu <larry.simiyu@gmail.com>
2019-07-29 09:55:49 -0700

    property image get endpoint

--- api/routes/propertyImg-router.js
+++ api/routes/propertyImg-router.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+// get list of images
+router.get("/", async (req, res) => {
+  try {
+    const image = await db.find();
+    res.status(200).json(image);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
 module.exports = router;
