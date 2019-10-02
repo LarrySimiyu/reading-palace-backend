e79fae9b12780e06aab6e5be5eb13391dbeab22a
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-02 16:47:34 -0700

    film info migration created

--- data/migrations/20190715223112_film_info.js
+++ data/migrations/20190715223112_film_info.js
@@ -2,6 +2,8 @@ exports.up = function(knex) {
   return knex.schema.createTable("film_info", tbl => {
     tbl.increments();
     tbl.string("Title", 100).notNullable();
+    tbl.string("Author", 100).notNullable();
+    tbl.string("Co_Author", 100);
     tbl.string("Description", 1000).notNullable();
     tbl.string("Category", 25).notNullable(); // category tags? - find by category tags
   });
