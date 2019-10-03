2a1b4a8c365149430ca4c8470ac08495eb061d8b
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-03 14:32:09 -0700

    film info seed created

--- data/seeds/film_info.js
+++ data/seeds/film_info.js
@@ -1,13 +1,34 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("table_name")
+  return knex("film_info")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
+      return knex("film_info").insert([
+        {
+          id: 1,
+          title: "Suits",
+          author: "Larry Simiyu",
+          co_author: "Teyonna Bosty",
+          description: "Greatest screenplay of all time",
+          category: "Horror"
+        },
+        {
+          id: 2,
+          title: "Crips",
+          author: "Bobby Drew",
+          co_author: "Finius Bosty",
+          description: "Greatest screenplay of all time",
+          category: "Romance"
+        },
+        {
+          id: 3,
+          title: "Bloody Hell Mate",
+          author: "Pippy Long Pew",
+          co_author: "Big Bootius",
+          description: "On god we gon get you a girl bro",
+          category: "Action"
+        }
       ]);
     });
 };
