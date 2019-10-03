c0a1a751f7dc98818ef9777945822f4e8e30254b
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-02 18:13:12 -0700

    description type changed to text

--- data/migrations/20190715223112_film_info.js
+++ data/migrations/20190715223112_film_info.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
     tbl.string("Title", 100).notNullable();
     tbl.string("Author", 100).notNullable();
     tbl.string("Co_Author", 100);
-    tbl.string("Description", 1000).notNullable();
+    tbl.text("Description", 1000).notNullable();
     tbl.string("Category", 25).notNullable(); // category tags? - find by category tags
   });
 };
