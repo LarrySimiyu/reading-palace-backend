b10ba49753ade6104ca0b658cdbf8930b9e5c644
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-03 19:49:06 -0700

    hitlist error fixed

--- data/seeds/hitlist.js
+++ data/seeds/hitlist.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("table_name")
+  return knex("hit_list")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("table_name").insert([
+      return knex("hit_list").insert([
         {
           id: 1,
           title: "One and Only"
