6115640cb99a6a88f832a88d308b6887d60def98
LarrySimiyu <larry.simiyu@gmail.com>
2019-10-09 21:02:58 -0700

    re changed

--- api/middleware/serversetup.js
+++ api/middleware/serversetup.js
@@ -2,20 +2,21 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require("morgan");
-const server = express();
 // rouuters
 
 const userRouter = require("../routes/user-router");
 const filmInfoRouter = require("../routes/filmInfo-router");
 const hitListRouter = require("../routes/hitList-router");
 const screenplayImgRouter = require("../routes/screenplayImage-router");
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
-server.use(morgan("dev"));
-server.use("/api/user", userRouter);
-server.use("/api/hitList", hitListRouter);
-server.use("/api/filmInfo", filmInfoRouter);
-server.use("/api/screenplayImage", screenplayImgRouter);
+
 // exports
-module.exports = server;
+module.exports = server => {
+  server.use(helmet());
+  server.use(express.json());
+  server.use(cors());
+  server.use(morgan("dev"));
+  server.use("/api/user", userRouter);
+  server.use("/api/hitList", hitListRouter);
+  server.use("/api/filmInfo", filmInfoRouter);
+  server.use("/api/screenplayImage", screenplayImgRouter);
+};
--- api/server.js
+++ api/server.js
@@ -4,10 +4,8 @@ const configMiddleware = require("./middleware/serversetup");
 const server = express();
 configMiddleware(server);
 
-server.get("/", async (req, res) => {
-  await res.status(200).json({
-    message: "Server up and running"
-  });
+server.get("/", (req, res) => {
+  res.send("Hello its working");
 });
 
 module.exports = server;
